76b439d993562e4d825e61c6353cb35ff5cbe10a
Erzhan2101 <erzhan210198@gmail.com>
2021-09-14 04:59:45 +0600

    hero

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import {BrowserRouter as Router, Route} from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Hero from "./views/Hero";
 
 
 function App() {
     return (
         <Router>
             <Header/>
+            <Hero/>
             <Footer/>
         </Router>
     );
---
+++ src/views/Hero/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./style.css"
+
+const Hero = () => {
+    return (
+        <main>
+            <div className="hero container">
+                <h1 className="hero-title">News portal V8</h1>
+                <button className="hero-btn">Зарегистироваться</button>
+            </div>
+        </main>
+    );
+};
+
+export default Hero;
